b0836864103c4d6ed07e50900bba4c34e81fb27e
Falish Dureja <durejafalish@gmail.com>
2024-07-18 22:44:41 +0530

    Logger added

--- logger.py
+++ logger.py
@@ -5,4 +5,44 @@
     @Created on: 18-07-2024
 """
 
+import sys
 import logging
+
+log_level_map = {
+    "INFO": logging.INFO,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL,
+    "DEBUG": logging.DEBUG
+}
+
+class FunkyLogger:
+    def __init__(self, logPath) -> None:
+        self._logName = "MAIN"
+        self._logger = self.create_logger(name=self._logName)
+        sHandler = logging.StreamHandler(sys.stdout)
+        self._logger.addHandler(sHandler)
+        sHandler.setFormatter(self.formatter)
+        self._logger.info("Logger Created SuccessFully!!")
+    
+    def create_logger(self, name):
+        logger = logging.getLogger(name)
+        self.name = name
+        logger.setLevel(logging.INFO)
+        self.formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+        return logger
+
+    def get_logger(self, name=None):
+        if not name or name == self._logName: 
+            name = self._logName
+            return self.logger
+        else:
+            return self.create_logger(name)
+    
+    def add_file_handler(self, logger, logPath, logName):
+        fHandler = logging.FileHandler(f"{logPath}\\{logName}.log", mode='w')
+        fHandler.setFormatter(self.formatter)
+        logger.addHandler(fHandler)
+
+    def set_level(self, logger, level):
+        logger.setLevel(log_level_map.get(level, logging.INFO))
+        
--- tester_main.py
+++ tester_main.py
@@ -5,22 +5,46 @@
 import os
 import argparse
 import yamlParser
+import logger
 
 class Tester:
     def __init__(self) -> None:
-        cli_args = self.argsParser()
-        self.yamlParser = yamlParser()
+        self.args = self.argsParser()
+        self.yamlParser = yamlParser.yamlParser(self.configFile)
+        self.logger = self.getLogger(name="tester")
+        self.logger.info("Main Tester Started!!!")
 
     def argsParser(self):
+        """
+            Argument parser for the main tester file.
+        """
         parser = argparse.ArgumentParser()
         parser.add_argument("--config", help="Device Config File containing system devices information", default="c:\\Automation\\device_config.yaml")
         parser.add_argument("--test", help="Test Case name that needs to be executed")
         parser.add_argument("--loops", help="Number of loops to be executed for given test case. Note: Only valid if --test arg is given", default=1, type=int)
+        parser.add_argument("--log", help="change the logging path. Note: Default path is in c:\\automation\\funkyLogs", default="C:\\Automation\\FunkyLogs")
         
         args = parser.parse_args()
 
         self.configFile = args.config
+        self.logPath = args.log
         if args.test:
             tests = args.test.split(",")
             self.testCaseList = tests
 
+    def getLogger(self, name, level="INFO"):
+        """
+            Create new or return the existing logger.
+            @args:
+                name: name of the logger
+            @return: logger object
+        """
+        self.fLogger = logger.FunkyLogger(self.logPath)
+        flog = self.fLogger.create_logger(name)
+        self.fLogger.add_file_handler(logger=flog, logPath=self.logPath, logName=self.fLogger.name)
+        print(flog)
+        self.fLogger.set_level(logger=flog, level=level)
+        return flog
+
+if __name__ == "__main__":
+    tester = Tester()
